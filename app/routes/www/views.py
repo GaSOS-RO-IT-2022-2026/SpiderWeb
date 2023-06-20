107929314110fabf258ee93728c1150d1ae0aa86
HyScript7 <hyscript7@seznam.cz>
2023-06-20 11:38:08 +0200

    Add debug template render route

--- app/routes/www/views.py
+++ app/routes/www/views.py
@@ -1,5 +1,16 @@
+from flask import render_template, request
+
 from . import www
+
 
 @www.route("/")
 async def root():
     return "Hello World!"
+
+
+@www.route("/debug/<template>")
+async def root_debug(template: str):
+    try:
+        return render_template(template + ".html", **request.args)
+    except Exception as e:
+        return f"Exception raised: {e}"
