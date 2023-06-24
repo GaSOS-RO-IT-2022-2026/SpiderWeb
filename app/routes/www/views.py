11eb51bb43d575483c67622fe224153ad57b2e27
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-24 10:58:20 +0200

    Start rewrite to flask templates

--- app/routes/www/views.py
+++ app/routes/www/views.py
@@ -14,3 +14,9 @@ async def root_debug(template: str):
         return render_template(template + ".html", **request.args)
     except Exception as e:
         return f"Exception raised: {e}"
+@www.route("/debug/<folder>/<template>")
+async def root_debug_sub(folder: str, template: str):
+    try:
+        return render_template(folder + "/" + template + ".html", **request.args)
+    except Exception as e:
+        return f"Exception raised: {e}"
